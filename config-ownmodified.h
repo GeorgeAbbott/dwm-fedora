315a6879a66955742e79a8e70077a1bf814598f2
self <self@sateoki.xyz>
2022-01-08 14:09:46 +0000

    Added all

--- config-ownmodified.h
+++ config-ownmodified.h
@@ -5,21 +5,46 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char *fonts[]          = { "Operator Mono:size=11" };
+static const char dmenufont[]       = "Operator Mono:size=11";
+static const char col_gray1[]       = "#1f1f1f"; 
+static const char col_gray2[]       = "#fcfcfc"; // used for border of unselected window
+static const char col_gray3[]       = "#b0b1b1";
+static const char col_gray4[]       = "#fcfcfc";
+static const char col_cyan[]        = "#002635";
+
+/* User Defined Colours */
+static const char col_atlas_bg[]    = "#002635";
+static const char
+          col_atlas_bright_black[]  = "#6c8b91";
+static const char
+          col_atlas_cyan[]          = "#14747e";
+static const char 
+          col_atlas_white[]         = "#a1a19a";
+static const char
+          col_atlas_bright_white[]  = "#fafaf8";
+/* Defining Color Aliases */
+#define NormFg                        col_atlas_white  // Text for dwm tags (e.g. 1-9), dwmstatus.
+#define SelFg                         col_atlas_bright_white  // Text in middle displaying active window.
+#define NormBg                        col_atlas_bg // Background for dwm tags, dwmstatus, etc.
+#define SelBg                         col_atlas_cyan // Background for middle displaying active window.j
+#define NormBorder                    "#ff00ff" // Border for unselected window.
+#define SelBorder                     "#00ffff" // Border for selected window.
+
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { NormFg,    NormBg,    NormBorder }, 
+	[SchemeSel]  = { SelFg,     SelBg,     SelBorder  },
 };
+/*
+ * Border is the border of windows; SchemeNorm when unselected and SchemeSel when selected.
+ *
+ *
+ */
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "T", "2", "3", "4", "5", "6", "7", "8", "F" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -27,7 +52,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "alacritty",NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
@@ -57,7 +82,9 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "alacritty", NULL };
+static const char *firefoxcmd[] = { "firefox", NULL }; 
+static const char *stcmd[] = { "st", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -94,6 +121,12 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+
+    // Own added
+    { MODKEY,                       XK_z,       spawn,          {.v = dmenucmd } },
+    { MODKEY,                       XK_s,       spawn,          {.v = firefoxcmd } },
+    { MODKEY,                       XK_c,       spawn,          {.v = termcmd } },
+    { MODKEY,                       XK_q,       spawn,          {.v = stcmd } }
 };
 
 /* button definitions */
